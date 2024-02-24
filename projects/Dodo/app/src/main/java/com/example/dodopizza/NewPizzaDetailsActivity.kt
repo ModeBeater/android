a9e6fd3402bc10f20b27693c65aafdad08cfad3b
ModeBeater <tashimov_nurh@mail.ru>
2024-02-24 16:16:26 +0600

    added bonus task

---
+++ projects/Dodo/app/src/main/java/com/example/dodopizza/NewPizzaDetailsActivity.kt
@@ -0,0 +1,32 @@
+package com.example.dodopizza
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.bumptech.glide.Glide
+class NewPizzaDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_activity_pizza_details)
+
+        val pizza = intent.getSerializableExtra("newPizza") as Pizza
+        val backButton: ImageButton = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            finish()
+        }
+        val pizzaImageView: ImageView = findViewById(R.id.pizzaImageView)
+        val pizzaNameTextView: TextView = findViewById(R.id.pizzaNameTextView)
+        val pizzaDescriptionTextView: TextView = findViewById(R.id.pizzaDescriptionTextView)
+        val pizzaIngredientsTextView: TextView = findViewById(R.id.pizzaIngredientsTextView)
+        val pizzaPriceTextView: TextView = findViewById(R.id.pizzaPriceTextView)
+        Glide.with(this).load(pizza.imageUrl).placeholder(R.drawable.placeholder_image).into(pizzaImageView)
+//        pizzaImageView.setImageResource(pizza.imageResourceId)
+        pizzaNameTextView.text = pizza.name
+        pizzaDescriptionTextView.text = pizza.description
+        pizzaIngredientsTextView.text = pizza.ingredients
+        pizzaPriceTextView.text = "Price: $${pizza.price}"
+    }
+}
--- projects/Dodo/app/src/main/java/com/example/dodopizza/PizzaAdapter.kt
+++ projects/Dodo/app/src/main/java/com/example/dodopizza/PizzaAdapter.kt
@@ -12,28 +12,44 @@ import com.bumptech.glide.Glide
 import org.w3c.dom.Text
 import java.io.Serializable
 
-class PizzaAdapter(private var pizzas: List<Pizza>) : RecyclerView.Adapter<PizzaAdapter.PizzaViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PizzaViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val itemView = inflater.inflate(R.layout.pizza_item, parent, false)
-        return PizzaViewHolder(itemView)
+class PizzaAdapter(private var pizzas: List<Pizza>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    companion object {
+        private const val VIEW_TYPE_REGULAR_PIZZA = 0
+        private const val VIEW_TYPE_NEW_PIZZA = 1
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_REGULAR_PIZZA -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.pizza_item, parent, false)
+                RegularPizzaViewHolder(view)
+            }
+            VIEW_TYPE_NEW_PIZZA -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.pizza_item_new, parent, false)
+                NewPizzaViewHolder(view)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
-    override fun onBindViewHolder(holder: PizzaViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val pizza = pizzas[position]
-        holder.bind(pizza)
+        when (getItemViewType(position)) {
+            VIEW_TYPE_REGULAR_PIZZA -> (holder as RegularPizzaViewHolder).bind(pizza)
+            VIEW_TYPE_NEW_PIZZA -> (holder as NewPizzaViewHolder).bind(pizza)
+        }
     }
 
     override fun getItemCount(): Int {
         return pizzas.size
     }
-
+    override fun getItemViewType(position: Int): Int {
+        return if (position < 3) VIEW_TYPE_NEW_PIZZA else VIEW_TYPE_REGULAR_PIZZA
+    }
     fun submitList(newList: List<Pizza>) {
         pizzas = newList
         notifyDataSetChanged()
     }
-
-    inner class PizzaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class NewPizzaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val nameTextView: TextView = itemView.findViewById(R.id.pizzaNameTextView)
         private val ingredientsTextView: TextView = itemView.findViewById(R.id.pizzaIngredientsTextView)
         private val imageView: ImageView = itemView.findViewById(R.id.pizzaImageView)
@@ -47,7 +63,26 @@ class PizzaAdapter(private var pizzas: List<Pizza>) : RecyclerView.Adapter<Pizza
                 .placeholder(R.drawable.placeholder_image)
                 .into(imageView)
             itemView.setOnClickListener {
-                // Handle pizza item click
+                val intent = Intent(itemView.context, NewPizzaDetailsActivity::class.java)
+                intent.putExtra("newPizza", pizza)
+                itemView.context.startActivity(intent)
+            }
+        }
+    }
+    inner class RegularPizzaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val nameTextView: TextView = itemView.findViewById(R.id.pizzaNameTextView)
+        private val ingredientsTextView: TextView = itemView.findViewById(R.id.pizzaIngredientsTextView)
+        private val imageView: ImageView = itemView.findViewById(R.id.pizzaImageView)
+        private val price: TextView = itemView.findViewById(R.id.pizzaPriceTextView)
+        fun bind(pizza: Pizza) {
+            nameTextView.text = pizza.name
+            ingredientsTextView.text = pizza.ingredients
+            price.text = "Price: $${pizza.price}"
+            Glide.with(itemView.context)
+                .load(pizza.imageUrl)
+                .placeholder(R.drawable.placeholder_image)
+                .into(imageView)
+            itemView.setOnClickListener {
                 val intent = Intent(itemView.context, PizzaDetailsActivity::class.java)
                 intent.putExtra("pizza", pizza)
                 itemView.context.startActivity(intent)
